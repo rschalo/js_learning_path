dabb4a3da22d2984c9b17add6ec79131ccb463c4
rschalo <reed0schalo@gmail.com>
2019-11-25 17:31:50 -0800

    Additional practice with alerts

--- conditional_practice.js
+++ conditional_practice.js
@@ -1,10 +1,18 @@
-age = prompt("Hey! Are you old enough to be in this bar? (hint: enter at least 95 for a surprise!)");
+perfect_squares = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+var age = prompt("Hey! Are you old enough to be in this bar? (hint: enter at least 95 for a surprise!)");
+
 if (age < 18){
     alert("Get out of here, I can't have kids ruining the place.");
+} else if (age >= 95){
+    alert("Wow. Mad respect to this crazy old guy. Hey, hey Charles! Come check out this relic who still rocks harder than Steven Tyler!");
+} else if (age == 21){
+    alert("Happy 21st birthday! Your first drink is on the house.");
+} else if (age % 2 === 1){
+    alert("Having an odd year? Your age is!");
+} else if (perfect_squares.includes(age)){
+    alert("WOW! Your age is a perfect square!");
+} else if (age < 0){
+    alert("Error, please enter a real age");
 } else if (age >= 18 && age < 21) {
-    alert("Come on in but you've got to wear this bracelet so you don't get served.");
-} else if (age >= 95) {
-    alert("Wow. Mad respect to this crazy old guy. Hey, hey Charles! Come check out this relic who still rocks harder than Steven Tyler!")
-} else {
-    alert("You're good to go, here's your drink tickets. Have fun!");
-}
+    alert("Come on in but you've got to wear this bracelet so you don't get served.")
+};
