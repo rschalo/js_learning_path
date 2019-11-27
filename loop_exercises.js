997671666313898873d36871e201ecc82a786ba5
rschalo <reed0schalo@gmail.com>
2019-11-27 12:45:33 -0800

    Only broke the browser once

--- loop_exercises.js
+++ loop_exercises.js
@@ -1,6 +1,37 @@
-var num = 1;
+/*var num = 1;
 
 while (num <= 10) {
     console.log(num);
     num += 2;
-}
+}*/
+
+/*var neg_counter = -10;
+console.log(neg_counter)
+while (neg_counter < 19){
+    neg_counter += 1;
+    console.log(neg_counter);
+}*/
+
+/* var even_counter = 10
+while (even_counter < 40){
+    console.log(even_counter);
+    even_counter += 2;
+}; */
+
+/* var odd_counter = 300;
+while (odd_counter < 333){
+    if ((odd_counter % 2) === 0 && (odd_counter % 3) === 0){
+        odd_counter += 1;
+    } else {
+        odd_counter += 2;
+    };
+    console.log(odd_counter);
+}; */
+
+/* var five_and_three_counter = 5;
+while (five_and_three_counter < 50){
+    if ((five_and_three_counter % 5) === 0 && (five_and_three_counter % 3) === 0){
+        console.log(five_and_three_counter);
+    };
+    five_and_three_counter += 1;
+}; */
