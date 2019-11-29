28275875f895ebf8d4194e0c8d95dac8f94d50fd
rschalo <reed0schalo@gmail.com>
2019-11-29 13:23:53 -0800

    Using while to get correct prompts

---
+++ annoyotron_loop.js
@@ -0,0 +1,5 @@
+var answer = prompt("Are we there yet?");
+while((answer.indexOf("yes") === -1) && (answer.indexOf("yea") === -1)){
+    var answer = prompt("Are we there yet?")
+}
+alert("YAY - WE FINALLY MADE IT!")
