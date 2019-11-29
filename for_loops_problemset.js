a06f60e47799a73aee4ad3fe17ed779e746d93ad
rschalo <reed0schalo@gmail.com>
2019-11-29 13:32:45 -0800

    Same problems as while loop pracitce but with for loops

---
+++ for_loops_problemset.js
@@ -0,0 +1,25 @@
+//print all numbers between -10 and 19
+for(i=-10;i<=19;i++){
+    console.log(i);
+}
+
+//print all even numbers between 10 and 40
+for(i=10;i<=40;i++){
+    if((i%2)===0){
+        console.log(i);
+    };
+}
+
+//print all odd numbers between 300 and 333
+for(i=300;i<=333;i++){
+    if((i%2)!==0){
+        console.log(i);
+    };
+}
+
+//print all numbers divisble by 5 and 3 between 5 and 50
+for(i=5;i<=50;i++){
+    if((i%5)===0 && (i%3)===0){
+        console.log(i);
+    };
+}
