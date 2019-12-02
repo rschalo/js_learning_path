de4d980127c0ba074b16b437106d9cd52d832bf9
Reed Schalo <reed0schalo@gmail.com>
2019-12-02 11:47:33 -0800

    Remove empty array element when element is deleted

--- todo_app.js
+++ todo_app.js
@@ -1,11 +1,25 @@
 let to_do_list = [];
 let response = "";
 let new_item = "";
+let delete_item = "";
 const greet = function(){
-    response = prompt("Thank you for using ToDo List! What would you like to do?");
-    if(response === "New"){
-        new_item = prompt("What item would you like to add?");
-        to_do_list.push(new_item);
+    while (response != "Exit"){
+        response = prompt("Thank you for using ToDo List! What would you like to do?");
+        if(response === "New"){
+            new_item = prompt("What item would you like to add?");
+            to_do_list.push(new_item);
+        } else if (response === "List"){
+            console.log(to_do_list);
+        } else if (response === "Delete"){
+            delete_item = prompt("What item would you like to delete?");
+            deletion = to_do_list.indexOf(delete_item);
+            delete to_do_list[deletion];
+            to_do_list = to_do_list.filter(function(el) {
+                return el != "";
+            });
+        } else {
+            break;
+        };
     }
 }
 
