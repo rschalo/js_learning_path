b179f32d86bf96e27aeb1faa750847e5e9be9238
Reed Schalo <reed0schalo@gmail.com>
2019-12-02 14:41:13 -0800

    Comments to clarify code

--- todo_app.js
+++ todo_app.js
@@ -3,24 +3,28 @@ let response = "";
 let new_item = "";
 let delete_item = "";
 const greet = function(){
-    while (response != "Exit"){
+    while (response !== "Exit"){
         response = prompt("Thank you for using ToDo List! What would you like to do?");
         if(response.toLowerCase() === "new"){
             new_item = prompt("What item would you like to add?");
             to_do_list.push(new_item);
+            //prompt user for new entry in todo list
         } else if (response.toLowerCase() === "list"){
             console.log(to_do_list);
+            //prompt user to view todo list in console
         } else if (response.toLowerCase() === "delete"){
             delete_item = prompt("What item would you like to delete?");
-            deletion = to_do_list.indexOf(delete_item);
+            deletion_index = to_do_list.indexOf(delete_item);
             delete to_do_list[deletion];
             to_do_list = to_do_list.filter(function(el) {
                 return el != "";
             });
+            //delete an item in the array based on text, not just index
         } else {
             break;
+            //exit while loop
         };
-    }
+    } console.log("Thanks for using this app!")
 }
 
-document.addEventListener("load", greet())
+document.addEventListener("DOMContentLoaded", greet())
