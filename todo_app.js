ef6c187b752c9d2dd74066c1ac7c38a3c2fbc579
Reed Schalo <reed0schalo@gmail.com>
2019-12-02 11:22:57 -0800

    Add prompts to save new to_do_list item

--- todo_app.js
+++ todo_app.js
@@ -1,3 +1,12 @@
-document.addEventListener("DOMContentLoaded", function(event){
-    alert("Hey this works!")
-});
+let to_do_list = [];
+let response = "";
+let new_item = "";
+const greet = function(){
+    response = prompt("Thank you for using ToDo List! What would you like to do?");
+    if(response === "New"){
+        new_item = prompt("What item would you like to add?");
+        to_do_list.push(new_item);
+    }
+}
+
+document.addEventListener("load", greet())
