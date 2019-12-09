1a45e1dc77bc99a3b2c2d4ad26a4a9cddcb4537a
rschalo <reed0schalo@gmail.com>
2019-12-08 21:29:22 -0800

    Test ternary operator and objects

---
+++ movieDB.js
@@ -0,0 +1,22 @@
+//Create an array of movie objects with properties: title, rating, hasWatched
+var myMovies = [
+    {name: "Avatar",
+     rating: 4.5,
+     hasWatched: true},
+    {name: "Frozen 2",
+     rating: 5,
+     hasWatched: true},
+    {name: "Twilight",
+     rating: 0,
+     hasWatched: false},
+    {name: "13th Amendment",
+     rating: 5,
+     hasWatched: false}
+]
+
+for(const movie of myMovies){
+    let watched = (movie.hasWatched ? "seen":"not seen");
+    let myRating = movie.rating;
+    let name = movie.name;
+    console.log("I have " + watched + " " + name + " and I give it " + myRating + " stars.");
+};
