a4970a494fabbe9a98d455638a1e332c241dea5e
rschalo <reed0schalo@gmail.com>
2019-12-12 23:02:09 -0800

    Learn DOM manipulation with for loops

---
+++ google_logo_experiment.js
@@ -0,0 +1,6 @@
+var logo = document.querySelector("#hplogo");
+logo.setAttribute("src", "http://www.eastcovettvets.co.uk/uploads/Animals/gingerkitten.jpg");
+
+logo.style.width = "500 px";
+logo.style.height = "100 px";
+logo.style.border = "2px solid purple";
