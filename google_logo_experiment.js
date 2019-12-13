73fb9367c23b79afff38109a26be7b97b3d9581c
rschalo <reed0schalo@gmail.com>
2019-12-12 23:03:06 -0800

    Update to for loops

--- google_logo_experiment.js
+++ google_logo_experiment.js
@@ -4,3 +4,12 @@ logo.setAttribute("src", "http://www.eastcovettvets.co.uk/uploads/Animals/ginger
 logo.style.width = "500 px";
 logo.style.height = "100 px";
 logo.style.border = "2px solid purple";
+
+var links = document.getElementsByTagName("a");
+
+//boilerplate for loop
+
+for(var i=0;i<links.length;i++){
+    links[i].setAttribute("target", "update");
+    links[i].getAttribute("target");
+}
