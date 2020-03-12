9a4e618b6a203490ac718c9bcdfe884712a8fd8e
Reed Schalo <reed0schalo@gmail.com>
2020-03-12 12:45:17 -0700

    Add edit & delete authorization to users

--- YelpCamp/v5/routes/campgrounds.js
+++ YelpCamp/v5/routes/campgrounds.js
@@ -63,13 +63,10 @@ router.get("/:id", function(req, res) {
 });
 
 // EDIT CAMPGROUND ROUTE
-router.get(`/:id/edit`, function(req, res){
-  Campground.findById(req.params.id, function(err, foundCampground){
-    if (err) {
-      res.redirect("/campgrounds");
-    } else {
-      res.render("campgrounds/edit", { campground: foundCampground });
-    }
+
+router.get(`/:id/edit`, confirmOwner, function(req, res) {
+  Campground.findById(req.params.id, function(err, foundCampground) {
+    res.render(`campgrounds/edit`, { campground: foundCampground });
   });
 });
 
@@ -90,21 +87,31 @@ router.put(`/:id`, (req, res) => {
 });
 
 // Destroy campground Route
-router.delete(`/:id`, function(req, res){
-  Campground.findByIdAndRemove(req.params.id, function(err){
-    if(err){
-      res.redirect(`/campgrounds`);
-    } else {
-      res.redirect(`/campgrounds`)
-    }
-  })
-})
+router.delete(`/:id`, confirmOwner, function(req, res) {
+  Campground.findByIdAndRemove(req.params.id, function(err) {
+    res.render(`campgrounds`);
+  });
+});
 
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   } else {
     res.redirect(`/login`);
+  }
+}
+
+function confirmOwner(req, res, next) {
+  if (req.isAuthenticated()) {
+    Campground.findById(req.params.id, function(err, foundCampground) {
+      if (foundCampground.author.id.equals(req.user._id)) {
+        next();
+      } else {
+        res.redirect(`back`);
+      }
+    });
+  } else {
+    res.redirect(`back`);
   }
 }
 
