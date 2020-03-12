f5bc328d8588bd9e305978a4570198c91643e1a4
Reed Schalo <reed0schalo@gmail.com>
2020-03-12 15:06:17 -0700

    Add edit route for comments

--- YelpCamp/v5/routes/comments.js
+++ YelpCamp/v5/routes/comments.js
@@ -40,6 +40,46 @@ router.post(`/`, isLoggedIn, function(req, res) {
   });
 });
 
+router.get(`/:comment_id/edit`, function(req, res) {
+  Comment.findById(req.params.comment_id, function(err, foundComment) {
+    if (err) {
+      res.redirect(`back`);
+    } else {
+      res.render(`comments/edit`, {
+        campground_id: req.params.id,
+        comment: foundComment
+      });
+    }
+  });
+});
+
+// COMMENT UPDATE ROUTE
+
+router.put(`/:comment_id`, function(req, res) {
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(
+    err,
+    updatedComment
+  ) {
+    if (err) {
+      res.redirect(`back`);
+    } else {
+      res.redirect(`/campgrounds/` + req.params.id);
+    }
+  });
+});
+
+router.delete(`/:comment_id`, function(req, res) {
+  Comment.findByIdAndRemove(req.params.comment_id, req.body.comment, function(
+    err
+  ) {
+    if (err) {
+      res.redirect(`back`);
+    } else {
+      res.render(`/campgrounds/` + req.params.id);
+    }
+  });
+});
+
 //middleware
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
